ee73753ada72fdab7233b1cc114f85507968c4c8
Mysan <mysanr@emea.roche.com>
2017-08-04 09:35:32 +0200

    added profiles and web.xmls

---
+++ src/main/java/app/components/Development.java
@@ -0,0 +1,20 @@
+package app.components;
+
+public class Development {
+
+    private String property;
+
+    public Development(){}
+
+    public Development(String property) {
+        this.property = property;
+    }
+
+    public String getProperty() {
+        return property;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+}
---
+++ src/main/java/app/components/Test.java
@@ -0,0 +1,20 @@
+package app.components;
+
+public class Test {
+
+    private String property;
+
+    public Test(){}
+
+    public Test(String property) {
+        this.property = property;
+    }
+
+    public String getProperty() {
+        return property;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+}
