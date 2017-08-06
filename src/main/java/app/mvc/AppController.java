1c301326bc3763065aaaf16aed32a5a4ceee5907
Mysan <mysanr@emea.roche.com>
2017-08-06 15:37:41 +0200

    added profiles and property files

--- src/main/java/app/mvc/AppController.java
+++ src/main/java/app/mvc/AppController.java
@@ -1,14 +1,19 @@
 package app.mvc;
 
-import org.springframework.context.annotation.Profile;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class AppController {
-    @RequestMapping("/test")
-    public String test(){
+    @Value("${profile.name}")
+    private String value;
+
+    @RequestMapping("/")
+    public String test(Model model){
+        System.out.println(value);
+        model.addAttribute("profile", value);
         return "view";
     }
 }
--- src/main/webapp/WEB-INF/jsp/view.jsp
+++ src/main/webapp/WEB-INF/jsp/view.jsp
@@ -1,1 +1,2 @@
-Hello world!
+${profile}
+${test}
