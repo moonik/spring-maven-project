cb8020c9159ea2746b765fd0752607af4b46ddb7
Mysan <mysanr@emea.roche.com>
2017-08-07 08:32:28 +0200

    fixed problem with web.xmls

--- src/main/java/app/mvc/AppController.java
+++ src/main/java/app/mvc/AppController.java
@@ -1,18 +1,22 @@
 package app.mvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Properties;
+
 @Controller
 public class AppController {
-    @Value("${profile.name}")
+    @Value("${profile}")
     private String value;
 
     @RequestMapping("/")
     public String test(Model model){
-        System.out.println(value);
         model.addAttribute("profile", value);
         return "view";
     }
